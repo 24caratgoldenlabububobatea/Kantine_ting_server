89b13daf689160d5ecb7b20092bf1680e3c11211
24caratgoldenlabububobatea <jonathanrasmussen2020@gmail.com>
2025-12-05 15:00:31 +0000

    added forms and defed get_conn so i can connect my sql to the python code

--- app.py
+++ app.py
@@ -1,26 +1,14 @@
-from flask import Flask, render_template_string, render_template
+from flask import Flask, render_template, redirect
 import mysql.connector
+from forms import RegisterForm, LoginForm
 
 app = Flask(__name__)
+app.secret_key = 'hemmlig_nokkel'
 
-conn = mysql.connector.connect(
-    host="localhost",
-    user="jonathan",
-    password="amongusishot34",
-    database="eksempel_db"
+def get_conn():
+    return mysql.connector.connect(
+        host="localhost",
+        user="jonathan",
+        password="amongusishot34",
+        database="eksempel_db"
     )
-
-@app.route('/')
-def index():
-
-    cur = conn.cursor()
-    
-    cur.execute("SELECT navn, alder FROM brukere;")
-    
-    row = cur.fetchall()
-    
-    
-    return render_template('index.html', brukere=row)
-
-if __name__ == "__main__":
-    app.run()
