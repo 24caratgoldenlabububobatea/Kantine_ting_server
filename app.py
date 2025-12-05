c4222b7d1378694b5cf8e8deb256356b05721bb4
24caratgoldenlabububobatea <jonathanrasmussen2020@gmail.com>
2025-12-05 15:14:20 +0000

    finished the routes and added the html files

--- app.py
+++ app.py
@@ -12,3 +12,56 @@ def get_conn():
         password="amongusishot34",
         database="eksempel_db"
     )
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegisterForm()  
+
+    if form.validate_on_submit():
+        navn = form.name.data
+        brukernavn = form.username.data
+        passord = form.password.data
+
+        conn = get_conn()
+        cur = conn.cursor()
+        cur.execute(
+            "INSERT INTO kunder (navn, brukernavn, passord) VALUES (%s, %s, %s)",
+            (navn, brukernavn, passord)
+        )
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        return redirect("/login")
+
+    
+    return render_template("register.html", form=form)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()  
+
+    # Sjekk om skjemaet er sendt inn og gyldig
+    if form.validate_on_submit():
+        brukernavn = form.username.data
+        passord = form.password.data
+
+        conn = get_conn()
+        cur = conn.cursor()
+        cur.execute(
+            "SELECT navn FROM kunder WHERE brukernavn=%s AND passord=%s",
+            (brukernavn, passord)
+        )
+        user = cur.fetchone() 
+        cur.close()
+        conn.close()
+
+        
+        if user:
+            return render_template("welcome.html", name=user[0])
+        else:
+            form.username.errors.append("Feil brukernavn eller passord")
+
+    return render_template("login.html", form=form)
+
+
